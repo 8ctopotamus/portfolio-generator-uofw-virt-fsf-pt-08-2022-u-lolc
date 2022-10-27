1aff3356f7ca6f870ad22b8e613f2e916b10e68c
Josh <zylo.codes@gmail.com>
2022-10-26 21:31:46 -0500

    use generateHTML and fs.writeFile

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-// * Your application should prompt the user for information like their name, location, bio, LinkedIn URL, and GitHub URL.
+import fs from "fs"
+import inquirer from 'inquirer'
+import generateHTML from './generateHTML.js'
+
+inquirer.prompt([
+  {
+    type: "input",
+    message: "What's your name?",
+    name: "name"
+  },
+  {
+    type: "input",
+    message: "What's your location?",
+    name: "location"
+  },
+  {
+    type: "input",
+    message: "Tell me about yourself",
+    name: "bio"
+  },
+  {
+    type: "input",
+    message: "What's your LinkedIn URL?",
+    name: "linkedinURL"
+  },
+  {
+    type: "input",
+    message: "What's your Github URL?",
+    name: "githubURL"
+  },
+])
+  .then(answers => {
+    console.log(answers)
+
+    // generate HTML string
+    const html = generateHTML(answers)
+    
+    // write (save) the file
+    fs.writeFile('./portfolio.html', html, error => {
+      if (error) throw error
+
+      console.log('HTML saved!')
+    })
+  })
+  .catch(error => console.log(error))
